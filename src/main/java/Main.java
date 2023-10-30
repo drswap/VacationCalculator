64509f218107339b19525dfc520089a06c0392ec
Swap <swap15.2014@gmail.com>
2023-10-30 23:04:53 +0300

    change

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,7 @@ import ru.netology.services.VacationCalculator.months.CalculationOfTheNumberOfMo
 public class Main {
     public static void main(String[] args) {
         CalculationOfTheNumberOfMonths service = new CalculationOfTheNumberOfMonths();
-        int income = 100000;
+        int income = 10000;
         int expenses = 60000;
         int threshold = 150000;
         int vacationMonths = service.calculate(income, expenses, threshold );
--- src/test/java/ru/netology/services/VacationCalculator/months/CalculationOfTheNumberOfMonthsTest.java
+++ src/test/java/ru/netology/services/VacationCalculator/months/CalculationOfTheNumberOfMonthsTest.java
@@ -1,28 +1,37 @@
 package ru.netology.services.VacationCalculator.months;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import ru.netology.services.VacationCalculator.months.CalculationOfTheNumberOfMonths;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//import ru.netology.services.VacationCalculator.months.CalculationOfTheNumberOfMonths;
 public class CalculationOfTheNumberOfMonthsTest {
-    @Test
-            public void trueVacationMonths (){
+    @ParameterizedTest
+    @CsvSource({
+            "3,100000,60000,150000",
+
+    })
+            public void calculate (int expected, int income, int expenses, int threshould){
     CalculationOfTheNumberOfMonths service = new CalculationOfTheNumberOfMonths();
-    int income = 100_000;
-    int expenses = 60_000;
-    int threshold = 150_000;
-    int vacationMonths = service.calculate(100_000, 60_000, 150_000 );
+    //int income = 100_000;
+   // int expenses = 60_000;
+    //int threshold = 150_000;
+    int actual = service.calculate(income, expenses,threshould);
         //System.out.println("Количество месяцев отдыха: " + vacationMonths);
 
-        Assertions.assertEquals(vacationMonths,threshold);
+        assertEquals(expected, actual);
     }
-    @Test
-    public void falseVacationMonths () {
-        CalculationOfTheNumberOfMonths service = new CalculationOfTheNumberOfMonths();
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 50_000;
-        int vacationMonths = service.calculate(100_000, 60_000, 50_000 );
+    //@Test
+    //public void falseVacationMonths () {
+      //  CalculationOfTheNumberOfMonths service = new CalculationOfTheNumberOfMonths();
+        //int income = 100_000;
+        //int expenses = 90_000;
+        //int threshold = 20_000;
+        //int vacationMonths = service.calculate(100_000, 60_000, 50_000 );
         //System.out.println("Количество месяцев отдыха: " + vacationMonths);
 
-        Assertions.assertEquals(vacationMonths,threshold);
+       // Assertions.assertEquals(vacationMonths,threshold);
     }
-}
+
